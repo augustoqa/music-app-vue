a3da467b6678a538e7291ea2904a4d0551004134
Augusto Quisquinay <augustoacual@gmail.com>
2023-06-19 18:11:21 -0600

    Disable multi word components name vue rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,18 +6,21 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   overrides: [
     {
       files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
-      extends: ['plugin:cypress/recommended']
-    }
+      extends: ['plugin:cypress/recommended'],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   env: {
-    node: true
-  }
+    node: true,
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
 }
