0f965c898e6e4effbb1aaf07460cf268fe312e54
Augusto Quisquinay <augustoacual@gmail.com>
2024-03-31 12:13:11 -0600

    Add feature to register user in Firebase

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -106,6 +106,8 @@
 </template>
 
 <script>
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+
 export default {
   name: 'RegisterForm',
   data() {
@@ -129,15 +131,31 @@ export default {
     }
   },
   methods: {
-    register(values) {
+    async register(values) {
       this.reg_show_alert = true
       this.reg_in_submission = true
       this.reg_alert_variant = 'bg-blue-500'
       this.reg_alert_msg = 'Please wait! Your account is being created.'
 
+      let userCred = null
+      try {
+        userCred = await createUserWithEmailAndPassword(
+          getAuth(),
+          values.email,
+          values.password
+        )
+      } catch (error) {
+        this.reg_in_submission = false
+        this.reg_alert_variant = 'bg-red-500'
+        this.reg_alert_msg =
+          'An unexpected error occured. Please try again later.'
+        console.log(error)
+        return
+      }
+
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Success! Your account has been created.'
-      console.log(values)
+      console.log(userCred)
     },
   },
 }
