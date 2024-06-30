67a9845b2c0319ea3c52b5b5007f1203796aadad
Cesar Quisquinay <augustoacual@gmail.com>
2024-06-29 22:44:08 -0600

    Navigation Using Router Links

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
+import Manage from '@/views/Manage.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
+  { path: '/manage', component: Manage },
 ]
 
 const router = createRouter({
