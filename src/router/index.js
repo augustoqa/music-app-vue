2b395a1d102dec37d682319fe958d03bade806f2
Cesar Quisquinay <augustoacual@gmail.com>
2024-07-24 21:40:14 -0600

    Restrict Manage Page Using Meta Field

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 import Manage from '@/views/Manage.vue'
+import useUserStore from '@/stores/user'
 
 const routes = [
   { name: 'home', path: '/', component: Home },
@@ -13,6 +14,9 @@ const routes = [
     beforeEnter: (to, from, next) => {
       console.log('Manage Route Guard')
       next()
+    },
+    meta: {
+      requiresAuth: true,
     },
   },
   { path: '/manage', redirect: { name: 'manage' } },
@@ -26,9 +30,18 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  console.log('Global guard')
+  if (!to.meta.requiresAuth) {
+    next()
+    return
+  }
 
-  next()
+  const store = useUserStore()
+
+  if (store.userLoggedIn) {
+    next()
+  } else {
+    next({ name: 'home' })
+  }
 })
 
 export default router
