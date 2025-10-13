5696e855aceb806dfa3a54cf22b14883690b9404
Augustoa Acual <augustoacual@gmail.com>
2025-10-13 12:54:23 -0600

    Upload File - Editing a Song

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -18,9 +18,11 @@
           <div class="p-6">
             <!-- Composition Items -->
             <composition-item
-              v-for="song in songs"
+              v-for="(song, i) in songs"
               :key="song.docID"
               :song="song"
+              :updateSong="updateSong"
+              :index="i"
             />
           </div>
         </div>
@@ -58,6 +60,12 @@ export default {
 
       this.songs.push(song)
     })
+  },
+  methods: {
+    updateSong(i, values) {
+      this.songs[i].modified_name = values.modified_name
+      this.songs[i].genre = values.genre
+    },
   },
 }
 </script>
