4f9163c6f77b454244b1f5e4f5f9b779eedef1d1
Augustoa Acual <augustoacual@gmail.com>
2026-01-14 08:22:41 -0600

    Play Music - Update the Comments List

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -58,6 +58,7 @@
         </vee-form>
         <!-- Sort Comments -->
         <select
+          v-model="sort"
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
         >
           <option value="1">Latest</option>
@@ -69,7 +70,7 @@
   <!-- Comments -->
   <ul class="container mx-auto">
     <li
-      v-for="comment in comments"
+      v-for="comment in sortedComments"
       :key="comment.docID"
       class="p-6 bg-gray-50 border border-gray-200"
     >
@@ -103,10 +104,20 @@ export default {
       comment_alert_variant: 'bg-blue-500',
       comment_alert_message: 'Please wait! Your comment is being submitted',
       comments: [],
+      sort: '1',
     }
   },
   computed: {
     ...mapState(useUserStore, ['userLoggedIn']),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        if (this.sort === '1') {
+          return new Date(b.datePosted) - new Date(a.datePosted)
+        }
+
+        return new Date(a.datePosted) - new Date(b.datePosted)
+      })
+    },
   },
   async created() {
     const docRef = doc(db, 'songs', this.$route.params.id)
@@ -137,6 +148,8 @@ export default {
       }
 
       await addDoc(commentsCollection, comment)
+
+      this.getComments()
 
       this.comment_in_submission = false
       this.comment_alert_variant = 'bg-green-500'
