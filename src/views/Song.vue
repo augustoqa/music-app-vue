7a8bc5a2cb8715cf315abff93ee1dbfbd226ae11
Augustoa Acual <augustoacual@gmail.com>
2026-01-14 09:58:38 -0600

    Play Music - Storing the song in the State and install HowlerJS

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -8,6 +8,7 @@
     <div class="container mx-auto flex items-center">
       <!-- Play/Pause Button -->
       <button
+        @click.prevent="newSong(song)"
         type="button"
         class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
       >
@@ -96,8 +97,9 @@ import {
   where,
 } from 'firebase/firestore'
 import { commentsCollection, auth, songsCollection } from '../includes/firebase'
-import { mapState } from 'pinia'
+import { mapState, mapActions } from 'pinia'
 import useUserStore from '@/stores/user'
+import usePlayerStore from '@/stores/player'
 
 export default {
   name: 'Song',
@@ -144,6 +146,7 @@ export default {
     this.getComments()
   },
   methods: {
+    ...mapActions(usePlayerStore, ['newSong']),
     async addComment(values, { resetForm }) {
       this.comment_in_submission = true
       this.comment_show_alert = true
