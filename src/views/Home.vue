d5009bfea27fe7ea400aef28ec8d95fe803d8330
Augustoa Acual <augustoacual@gmail.com>
2026-01-19 21:49:50 -0600

    Directives - Registering a Directive Locally

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,7 +31,7 @@
       >
         <div
           class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
-          v-icon.right.yellow="'headphones-alt'"
+          v-icon-secondary="{ icon: 'headphones-alt', right: true }"
         >
           <span class="card-title">Songs</span>
           <!-- Icon -->
@@ -60,10 +60,15 @@ import {
 } from 'firebase/firestore'
 import { db } from '../includes/firebase'
 
+import IconSecondary from '@/directives/icon-secondary'
+
 export default {
   name: 'Home',
   components: {
     AppSongItem,
+  },
+  directives: {
+    'icon-secondary': IconSecondary,
   },
   data() {
     return {
