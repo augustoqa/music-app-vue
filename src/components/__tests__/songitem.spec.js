661f064c989504232c20888bde59cebcac1b296e
Augustoa Acual <augustoacual@gmail.com>
2026-01-30 22:36:13 -0600

    Testing - Stubbing Components

--- src/components/__tests__/songitem.spec.js
+++ src/components/__tests__/songitem.spec.js
@@ -1,6 +1,5 @@
 import SongItem from '@/components/SongItem.vue'
-import { shallowMount } from '@vue/test-utils'
-import { expect } from 'vitest'
+import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 
 describe('SongItem.vue', () => {
   test('render song.display_name', () => {
@@ -12,8 +11,15 @@ describe('SongItem.vue', () => {
       propsData: {
         song,
       },
+      global: {
+        components: {
+          'router-link': RouterLinkStub,
+        },
+      },
     })
 
-    expect(wrapper.text()).toContain(song.display_name)
+    const compositionAuthor = wrapper.find('.text-gray-500')
+
+    expect(compositionAuthor.text()).toBe(song.display_name)
   })
 })
